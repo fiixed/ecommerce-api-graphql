ec9ed3b62933ab9c00de3d23f74bf7e0ffefcd3e
Andrew Bell <fiixed@gmail.com>
2023-06-01 09:41:53 -0500

    modularize resolvers

--- server.js
+++ server.js
@@ -8,40 +8,14 @@ const { makeExecutableSchema } = require('@graphql-tools/schema');
 const typesArray = loadFilesSync('**/*', {
   extensions: ['graphql'],
 });
-const schema = makeExecutableSchema({
-  typeDefs: typesArray
-});
 
-const root = {
-  products: [
-    {
-      id: 'redshoe',
-      description: 'Red Shoe',
-      price: 42.12,
-    },
-    {
-      id: 'bluejean',
-      description: 'Blue Jeans',
-      price: 55.55,
-    },
-  ],
-  orders: [
-    {
-      date: '2005-05-05',
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: 'redshoe',
-            description: 'Old Red Shoe',
-            price: 45.11,
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ],
-};
+const resolversArray = loadFilesSync('**/*', {
+  extensions: ['resolvers'],
+});
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+  resolvers: resolversArray,
+});
 
 const app = express();
 
@@ -49,7 +23,6 @@ app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
